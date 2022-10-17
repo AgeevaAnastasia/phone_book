449c167372330f753d912ed66ff89dd8edf7d3cd
Anastasia Ageeva <anastasia.smirnova@gmail.com>
2022-10-17 13:58:26 +0300

    First working version

---
+++ user_interface.py
@@ -0,0 +1,11 @@
+from controller import button_click as bc
+print()
+print('Это телефонный справочник, импортирующий записи в .csv, .xml и .html форматы. Для начала работы нажмите Enter')
+input()
+
+bc()
+
+
+print()
+
+
